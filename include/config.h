7400b2cf29c4c54210d14e419f5e2a06af11bb44
GonesGui <guilhermercorrea20@hotmail.com>
2025-01-27 15:34:27 -0300

    attrs485

---
+++ include/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define FIRM_VERSION "1.0.606"
+
+#define ID_DISPOSITIVO "Celulamt100f"
+
+#define USB_BAUD_RATE 115200
+
+
+#define HX711_PIN_DT 23   
+#define HX711_PIN_SCK 22    
+
+#define SERIALPORT_BAUD_RATE 115200
+#define SERIALPORT 2
+// Defina os pinos RX, TX, DE e RE
+#define RS485_RX_PIN 16  // RX1
+#define RS485_TX_PIN 17  // TX1
+#define RS485_DE_PIN 4  // Pino DE (Driver Enable)
+#define RS485_RE_PIN 15  // Pino RE (Receiver Enable)
